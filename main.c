800f5d1b8ad6938c810fe7a89f5edf79fd905f23
TheProgxy <theprogxy@gmail.com>
2023-07-12 11:43:29 +0200

    extracted the shaders

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <string.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
+void processInput(GLFWwindow* window);
 
 int getShaderCompileStatus(unsigned int shaderId) {
     int status;
@@ -21,42 +21,28 @@ int getShaderLinkingStatus(unsigned int shaderId) {
     return status;
 }
 
-// char* getShaderData(const char* path) {
-//     char* shader_data = calloc(sizeof(char), 1);
-//     return shader_data;
-// }
-
-// settings
 #define SCR_WIDTH 800
 #define SCR_HEIGHT 600
 
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
 int main() {
-    const char path[] = "./include/shaders/vertex.hlsl";
-    char* vertexShaderData = readFile(path);
+    const char pathVertexShader[] = "./include/shaders/vertex.hlsl";
+    const char* vertexShaderData = readFile(pathVertexShader);
     
     if (vertexShaderData == NULL) {
         return -1;
     }
 
-    printf("DEBUG: vertex shader data:\n%s\n", vertexShaderData);
+    const char pathFragmentShader[] = "./include/shaders/fragment.hlsl";
+    const char* fragmentShaderData = readFile(pathFragmentShader);
+    
+    if (fragmentShaderData == NULL) {
+        return -1;
+    }
 
     glfwInit();
 
     // Create the window
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Demo", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Game", NULL, NULL);
     if (window == NULL) {
         printf("GLFW:CREATE_WINDOW:ERROR: Failed to create GLFW window\n");
         glfwTerminate();
@@ -76,7 +62,7 @@ int main() {
     char infoLog[512];
     // Retrieve the vertex shaders
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glShaderSource(vertexShader, 1, &vertexShaderData, NULL);
     glCompileShader(vertexShader);
 
     // Check for shader compile errors
@@ -87,7 +73,7 @@ int main() {
 
     // Retrieve the fragment shader
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentShaderData, NULL);
     glCompileShader(fragmentShader);
     
     // Check for shader compile errors
@@ -171,7 +157,7 @@ int main() {
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window) {
+void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, TRUE);
     }
@@ -180,7 +166,5 @@ void processInput(GLFWwindow *window) {
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
