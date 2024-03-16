05e4e55078b9ded739f712f7e4380d0f40d442d4
TheProgxy <theprogxy@gmail.com>
2024-03-16 11:35:32 +0100

    fix

--- include/utility/model.h
+++ include/utility/model.h
@@ -16,4 +16,42 @@ typedef struct Texture {
     const char* type;
 } Texture;  
 
+typedef struct Shader {
+    unsigned int vertex_shader;
+    unsigned int fragment_shader;
+} Shader;
+
+typedef struct Array {
+    void** struct_data;
+    unsigned int size;
+} Array;
+
+#define GET_ELEMENT(type, arr, index) *((type*) arr.struct_data[index])
+
+void append_array(Array* arr, void* data) {
+    arr -> struct_data = (void**) realloc(arr -> struct_data, sizeof(void*) * (arr -> size + 1));
+    (arr -> struct_data)[arr -> size] = data;
+    (arr -> size)++;
+    return;
+}
+
+Array allocate_arr() {
+    Array arr = { .size = 0 };
+    arr.struct_data = (void**) calloc(arr.size, sizeof(void*));
+    return arr;
+}
+
+typedef struct Mesh {
+    unsigned int VAO;
+    unsigned int VBO;
+    unsigned int EBO;
+    Array vertices;
+    Array textures;
+    Array indices;
+} Mesh;
+
+Mesh create_mesh(Array vertices, Array indices, Array textures);
+void draw(Shader* shader);
+void setupMesh();
+
 #endif //_MODEL_H_
